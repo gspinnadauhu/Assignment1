51d53cbba5424a618c528cd933ff9df522644d8f
gspinnadauhu <markusalang@gmail.com>
2016-08-19 19:29:54 -0500

    add

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,10 @@
 #Part 1 pollutantmean function
 pollutantmean<-function(directory,pollutant,id=1:332){
         directory<-paste("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1/",...,sep="")
-        pollutant<-
+        if(x=="sulfate"|"nitrate"){
+                pollutant<-x
+        }else{
+                pollutant<-NA
+        }
+        
 }
