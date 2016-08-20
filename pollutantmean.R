59c4181b0cd3990c59013651910a68a43711c12c
gspinnadauhu <markusalang@gmail.com>
2016-08-19 20:49:24 -0500

    first bit corrected

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,6 +1,12 @@
 #Part 1 pollutantmean function
 pollutantmean<-function(directory,pollutant,id=1:332){
-        directory<-paste("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1/",...,sep="")
+        directory<-function(...) {
+                args<-list(...)
+                x<-args[["directory"]]
+                paste0("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1/",x)
+        }
+#Above here is tested and correct                
+                
         if(x=="sulfate"|"nitrate"){
                 pollutant<-x
         }
