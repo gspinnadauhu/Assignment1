b80336caebd00d45dca9e50c4fef6c3e08bd1aee
gspinnadauhu <markusalang@gmail.com>
2016-08-19 19:38:48 -0500

    i think this is right

--- pollutantmean.R
+++ pollutantmean.R
@@ -3,8 +3,10 @@ pollutantmean<-function(directory,pollutant,id=1:332){
         directory<-paste("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1/",...,sep="")
         if(x=="sulfate"|"nitrate"){
                 pollutant<-x
-        }else{
-                pollutant<-NA
         }
-        
+        if(is.numeric(y) && y<333){
+                id<-y
+        }
+        read.csv(directory)
+        mean(pollutant[!is.na(pollutant)])
 }
