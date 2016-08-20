ba82a582a5964c6e33bb971d4546193a6f702aaa
gspinnadauhu <markusalang@gmail.com>
2016-08-19 19:12:53 -0500

    1stcommit

---
+++ pollutantmean.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1")
+#Part 1 pollutantmean function
+pollutantmean<-function(directory,pollutant,id=1:332){
+        directory<-paste("C:/Users/mlang2/Dropbox/Coursera/R Programming/Assignment1/",...,sep="")
+        pollutant
+}
