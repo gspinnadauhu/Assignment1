1a231194ad28820809a8be178cd9d6b7ae0fb146
gspinnadauhu <markusalang@gmail.com>
2016-08-21 18:28:58 -0500

    working part1

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,18 +1,29 @@
 #Part 1 pollutantmean function
 pollutantmean<-function(directory,pollutant,id=1:332){
+        total = 0
+        observations = 0
         if (basename(getwd())!=directory){
                 setwd(file.path(getwd(),directory))      
         }
         else{}
-        if (id<10){
-                data<-read.csv(paste0("0","0",as.character(id),".csv"))
+        for (i in id){
+                if (i<10){
+                        data<-read.csv(paste0("0","0",as.character(i),".csv"))
+                }
+                else if (i>=10&i<100){
+                        data<-read.csv(paste0("0",as.character(i),".csv"))
+                }
+                else {
+                        data<-read.csv(paste0(as.character(i),".csv"))
+                }
+                data=na.omit(data)
+                observations=observations+nrow(data)
+                if (pollutant=="nitrate"){
+                        total=total+sum(data$nitrate)
+                }
+                else {
+                        total=total+sum(data$sulfate)
+                }
         }
-        else if (id>=10&id<100){
-                data<-read.csv(paste0("0",as.character(id),".csv"))
-        }
-        else {
-                data<-read.csv(paste0(as.character(id),".csv"))
-        }
-
-        
+        return(total/observations)
 }
