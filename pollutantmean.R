26aaf44f0a5f277e3672802241eecb40d634691c
gspinnadauhu <markusalang@gmail.com>
2016-08-22 00:08:55 -0500

    part 3 works except it is including 9 NAs, not sure why yet

--- pollutantmean.R
+++ pollutantmean.R
@@ -50,3 +50,26 @@ complete<-function(directory,id=1:332){
         results<-data.frame(cbind(id,nobs))
         results
 }
+#Part3
+corr<-function(directory,threshold=0){
+        if (basename(getwd())!=directory){
+                setwd(file.path(getwd(),directory))      
+        }
+        correllations<-vector("numeric")
+        results<-complete(directory,1:332)
+        results<-subset(results,nobs>=threshold,select=c(id,nobs))
+        for (i in results$id){
+                if (i<10){
+                        data<-read.csv(paste0("0","0",as.character(i),".csv"))
+                }
+                else if (i>=10&i<100){
+                        data<-read.csv(paste0("0",as.character(i),".csv"))
+                }
+                else {
+                        data<-read.csv(paste0(as.character(i),".csv"))
+                }
+                data<-data[complete.cases(data),]
+                correllations<-append(correllations,cor(data$nitrate,data$sulfate))
+        }
+        correllations
+}
