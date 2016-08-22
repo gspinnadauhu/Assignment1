72ed33ce1b94460152d9248e1aacd6a14a74e095
gspinnadauhu <markusalang@gmail.com>
2016-08-21 22:04:41 -0500

    part 1& 2 complete

--- pollutantmean.R
+++ pollutantmean.R
@@ -32,7 +32,8 @@ complete<-function(directory,id=1:332){
         if (basename(getwd())!=directory){
                 setwd(file.path(getwd(),directory))      
         }
-        idvect<-id
+        #idvect<-id
+        nobs<-vector("numeric")
         for (i in id){
                 if (i<10){
                         data<-read.csv(paste0("0","0",as.character(i),".csv"))
@@ -43,9 +44,9 @@ complete<-function(directory,id=1:332){
                 else {
                         data<-read.csv(paste0(as.character(i),".csv"))
                 }
-                
-                compcases<-complete.cases(data)
-                results<-
+                compcase<-complete.cases(data)
+                nobs<-append(nobs,length(compcase[compcase==TRUE]))
         }
-        
+        results<-data.frame(cbind(id,nobs))
+        results
 }
